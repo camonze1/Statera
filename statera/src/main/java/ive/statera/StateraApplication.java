d8c43322c8362a21b0dbc38528cb905577706fd7
Camille <camille.launois9@etu.univ-lorraine.fr>
2023-10-06 11:17:45 +0200

    create project

---
+++ statera/src/main/java/ive/statera/StateraApplication.java
@@ -0,0 +1,13 @@
+package ive.statera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateraApplication.class, args);
+	}
+
+}
