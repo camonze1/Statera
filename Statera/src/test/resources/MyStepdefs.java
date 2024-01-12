a4d766b5acc5f1e3535817689e9799ef5647dc4b
Bastien <bastien.stouvenel1@etu.univ-lorraine.fr>
2024-01-12 10:26:45 +0100

    Essais de tests avec cucumber

---
+++ Statera/src/test/resources/MyStepdefs.java
@@ -0,0 +1,47 @@
+public class MyStepdefs {
+    @io.cucumber.java.en.Given("^l'utilisateur ouvre l'application$")
+    public void lUtilisateurOuvreLApplication() {
+    }
+
+    @io.cucumber.java.en.When("^le terrain est initialisé$")
+    public void leTerrainEstInitialise() {
+    }
+
+    @io.cucumber.java.en.Then("^le terrain devrait avoir une taille de (\\d+)x(\\d+)$")
+    public void leTerrainDevraitAvoirUneTailleDeX(int arg0, int arg1) {
+    }
+
+    @io.cucumber.java.en.Given("^l'utilisateur a ouvert l'application$")
+    public void lUtilisateurAOuvertLApplication() {
+    }
+
+    @io.cucumber.java.en.When("^l'utilisateur sélectionne un biome$")
+    public void lUtilisateurSelectionneUnBiome() {
+    }
+
+    @io.cucumber.java.en.And("^l'utilisateur clique sur un terrain libre$")
+    public void lUtilisateurCliqueSurUnTerrainLibre() {
+    }
+
+    @io.cucumber.java.en.Then("^le terrain devrait être mis à jour avec le nouveau biome$")
+    public void leTerrainDevraitEtreMisAJourAvecLeNouveauBiome() {
+    }
+
+    @io.cucumber.java.en.And("^le solde environnemental devrait être mis à jour$")
+    public void leSoldeEnvironnementalDevraitEtreMisAJour() {
+    }
+
+    @io.cucumber.java.en.When("^l'utilisateur clique sur \"([^\"]*)\"$")
+    public void lUtilisateurCliqueSur(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @io.cucumber.java.en.Then("^une fenêtre de statistiques devrait s'afficher$")
+    public void uneFenetreDeStatistiquesDevraitSAfficher() {
+    }
+
+    @io.cucumber.java.en.And("^les statistiques devraient être correctes en fonction du terrain$")
+    public void lesStatistiquesDevraientEtreCorrectesEnFonctionDuTerrain() {
+    }
+}
