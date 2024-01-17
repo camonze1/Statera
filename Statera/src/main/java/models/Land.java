b57c7702221b5e7c4737b7b26a90a3b3fdee5549
camonze <camille.launois54@gmail.com>
2024-01-17 14:10:57 +0100

    fix

--- Statera/src/main/java/models/Land.java
+++ Statera/src/main/java/models/Land.java
@@ -20,7 +20,7 @@ public class Land {
     for (int i = 0; i < line; i++) {
       ArrayList<Biome> lines = new ArrayList<Biome>();
       for (int j = 0; j < column; j++) {
-        int randomValue = random.nextInt(13);
+        int randomValue = random.nextInt(5);
         if (randomValue == 0) {
           lines.add(new FreeWasteland());
         } else {
